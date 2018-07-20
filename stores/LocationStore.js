041944c8633068b347ba3efa677a7a883549358e
tomholford <tomholford0@gmail.com>
2018-07-20 09:28:52 -0700

    Location management (#11)

--- stores/LocationStore.js
+++ stores/LocationStore.js
@@ -1,7 +1,8 @@
-import { action, observable } from 'mobx';
- import { computed } from 'mobx-react';
+import { action, observable, reaction } from 'mobx';
+import { computed } from 'mobx-react';
 import AutoStore from '../utils/AutoStore';
 
+const CACHE_THRESHOLD = 30 * 60 * 1000; /* 30 minutes, in ms */
 const DEFAULT_STORE = {
   "locations": [
     {
@@ -31,17 +32,16 @@ const DEFAULT_STORE = {
 class LocationStore {
 	@observable locations = [];
 
-	static initialize = function() {
-    const store = new this();
-    AutoStore(this, store, 'dashboardStore', DEFAULT_STORE);
+	static initialize = function(weatherApi) {
+    const store = new this(weatherApi);
+    AutoStore(store, 'dashboardStore', DEFAULT_STORE);
 
+    store.loadData();
     return store;
 	}
 
-  constructor(initialStore) {
-    if(initialStore) {
-      this.locations = initialStore.locations;
-    }
+  constructor(weatherApi) {
+    this.weatherApi = weatherApi;
   }
 
   @action
@@ -64,6 +64,68 @@ class LocationStore {
       this.locations.splice(index, 1);
     }
 	}
+
+  @action
+  addResponse = (location, key, data) => {
+    const index = this.locations.indexOf(location);
+    let found = this.locations[index];
+
+    if(!('dt' in data)) {
+      data['dt'] = (new Date).getTime();
+    }
+
+    found[key] = data;
+  }
+
+  @action
+  loadData = () => {
+    this.locations.forEach((location) => {
+      if(this.locationCache(location, 'current')) {
+        // do nothing
+        console.log(`current cached: ${location.name}`);
+      } else {
+        console.log(`current api call: ${location.name}`);
+        this.weatherApi.getCurrent(location).then((data) => {
+          this.addResponse(location, 'current', data);
+        })
+      }
+
+      if(this.locationCache(location, 'forecast')) {
+        // do nothing
+        console.log(`forecast cached: ${location.name}`);
+      } else {
+        console.log(`forecast api call: ${location.name}`);
+        this.weatherApi.getForecast(location).then((data) => {
+          this.addResponse(location, 'forecast', data);
+        })
+      }
+    })
+  }
+
+  locationCache = (location, key) => {
+    if(key in location && location[key]){
+      return this.locationShouldUpdate(location, key);
+    }else{
+      return false;
+    }
+  }
+
+  locationShouldUpdate = (location, key, delay = CACHE_THRESHOLD) => {
+    let lastUpdated;
+    if('dt' in location[key]) {
+      lastUpdated = new Date(location[key]['dt'] * 1000);
+    } else {
+      return true;
+    }
+    const currentDate = new Date;
+    const delta = Math.abs(currentDate - lastUpdated);
+    console.log(`current date: ${currentDate.toLocaleString()}`);
+    console.log(`${key} last updated: ${lastUpdated.toLocaleString()}`);
+    console.log(`delta: ${delta} ms`);
+    console.log(`delay: ${delay} ms`);
+    console.log(`shouldUpdate: ${delta > delay}`);
+    return delta > delay;
+  }
 }
 
 export default LocationStore;
