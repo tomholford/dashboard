4426d43a077f2290697502ac9d214a1d4b720728
tomholford <tomholford@users.noreply.github.com>
2018-09-14 11:24:05 -0700

    Prep for open source release

--- utils/Environment.js
+++ utils/Environment.js
@@ -3,3 +3,4 @@ export const DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const PRODUCTION = process.env.NODE_ENV === 'production';
 export const REVISION = process.env.REVISION;
 export const BUILT_AT = process.env.BUILT_AT;
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
